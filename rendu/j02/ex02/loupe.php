e006555e671ebbb18d4069df4853bc6d79b4b2df
Benjamin DURON <bduron@e2r9p20.42.fr>
2017-04-06 16:02:34 +0200

    j02.4

---
+++ rendu/j02/ex02/loupe.php
@@ -0,0 +1,33 @@
+<?php
+
+function callback($matches)
+{
+	$matches[0] = preg_replace_callback('/>(.+?)</s',
+		function ($matches2)
+		{
+			return strtoupper($matches2[0]);
+		},
+		$matches[0]);
+
+	$matches[0] = preg_replace_callback('/".+?"/',
+		function ($matches2)
+		{
+			return strtoupper($matches2[0]);
+		},
+		$matches[0]);
+	return ($matches[0]);
+}
+
+function main($argc, $argv)
+{
+	if ($argc != 2)
+		return (0);
+
+	$file = file_get_contents($argv[1]) or die("Failed to open file\n");
+	$test = preg_replace_callback('/<a.*?\/a>/s', "callback", $file);
+	echo $test;
+}
+
+main($argc, $argv);
+
+?>
--- rendu/j02/ex03/who.php
+++ rendu/j02/ex03/who.php
@@ -7,13 +7,15 @@ function main()
 	$user = get_current_user();
 	$utmpx_header = 'a256user/a4id/a32line/ipid/itype/I2time/a256host/i16pad';	
 	$offset = 628;
-	$fp = fopen('/var/run/utmpx', 'rb');
+	$fp = fopen('/var/run/utmpx', 'rb') 
+		or die ("utmpx file not found\n");
 	
 	while ((($utmpx = fread($fp, 628))))
 	{	
 		$who = unpack($utmpx_header, $utmpx);
 			
-		if (trim($who['user']) === trim($user))
+	
+		if (trim($who['type']) == 7 && trim($who['user']) === trim($user))
 			$array[] = array_map("trim", $who);
 	}
 	array_multisort($array);
