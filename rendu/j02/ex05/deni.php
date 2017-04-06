0ac2a6154d58878a34813a6295ccf9255d0b1395
Benjamin DURON <bduron@e2r9p20.42.fr>
2017-04-06 20:16:42 +0200

    j02.6

--- rendu/j02/ex05/deni.php
+++ rendu/j02/ex05/deni.php
@@ -1,53 +1,57 @@
 #!/usr/bin/php
 <?php
 
-
-function is_valid_label($argv, $labels)
+function is_valid($argv, $argc, $labels)
 {
-	
-
-
+	if ($argc != 3 or !array_key_exists($argv[2], $labels))
+		die();	
 }
 
 function create_hashtab($label, $csv, $labels, $argv)
 {
-
 	foreach ($csv as $line)
-		$$label[$line[$labels[$argv[2]]]] = $line[$labels[$label]]; 
+		$tmp[$line[$labels[$argv[2]]]] = $line[$labels[$label]]; 
 
-	return ($$label);
+	return ($tmp);
 }
 
-function main($argv, $argc)
+function get_csv($argv)
 {
-	$labels = ['nom' => 0, 'prenom' => 1, 'mail' => 2, 'IP' => 3, 'pseudo' => 4];
-
-	is_valid_label() or die ("wrong label\n"); // a completer
-
 	file_exists($argv[1]) 
-		or die ("Cannot find $argv[1]\n");
+		or die();
 
 	if (($fp = fopen($argv[1], 'r')) === false)
 		return (0);
 
 	while ($csv[] = fgetcsv($fp, 0, ';'));
+	array_pop($csv);	
+	array_shift($csv);	
+	fclose($fp);
+	return ($csv);
+}
 
-//	array_pop($csv);	
+function main($argv, $argc)
+{
+	$labels = ['nom' => 0, 'prenom' => 1, 'mail' => 2, 'IP' => 3, 'pseudo' => 4];
+	is_valid($argv, $argc, $labels);
 
-	print_r($csv);	
+	$csv = get_csv($argv);
 
-	
+	$nom = create_hashtab("nom", $csv, $labels, $argv);	
+	$prenom = create_hashtab('prenom', $csv, $labels, $argv);	
+	$mail = create_hashtab('mail', $csv, $labels, $argv);	
+	$IP = create_hashtab('IP', $csv, $labels, $argv);	
+	$pseudo = create_hashtab('pseudo', $csv, $labels, $argv);	
 
-	$nom = create_hashtab('nom', $csv, $labels, $argv);	
-	$prenom = create_hashtab('nom', $csv, $labels, $argv);	
-	$mail = create_hashtab('nom', $csv, $labels, $argv);	
-	$IP = create_hashtab('nom', $csv, $labels, $argv);	
-	$pseudo = create_hashtab('nom', $csv, $labels, $argv);	
-	print_r($nom);
+	while(1)
+	{
+		print("Entrez votre commande: ");
+		if (($cmd = fgets(STDIN)) === false)
+			break;	
+		eval($cmd); 
+	}
+	echo "\n";	
 
-
-	// correct error 
-	fclose($argv[1]);
 }
 
 main($argv, $argc);
