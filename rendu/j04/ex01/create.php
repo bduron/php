42b9a22d6e6c9cf9d3a5e76e9ab6eee116b714fe
Benjamin Duron (linux) <bduron@student.42.fr>
2017-04-08 12:06:26 +0200

    j04

--- rendu/j04/ex01/create.php
+++ rendu/j04/ex01/create.php
@@ -1,5 +1,13 @@
 <?php
-$passwd_file = 'private/passwd';
+$passwd_file = '../htdocs/private/passwd'; // GOOD PATH ?
+
+function is_existing_account($account, $login)
+{
+	foreach ($account as $check)
+		if ($check['login'] === $login)
+			return true;
+	return false;
+}
 
 
 /******* Check _POST values ********/
@@ -15,22 +23,23 @@ if ($_POST['login'] == '' || $_POST['passwd'] == '' || $_POST['submit'] !== 'OK'
 if (file_exists($passwd_file))
 {
 	$account = unserialize(file_get_contents($passwd_file));
-	if (1) //// CHECK SI LE COMPTE EXISTE DEJA
+	if (is_existing_account($account, $_POST['login']) == false)
 	{
-		$account[] = ['login' => $_POST['login'], 'passwd' => hash('sha256' ,$_POST['passwd'])]; 
+		$account[] = ['login' => $_POST['login'], 'passwd' => hash('whirlpool' ,$_POST['passwd'])]; 
 		file_put_contents($passwd_file, serialize($account))
 			or die("Can't write file\n");	
-	}	
+		echo "OK\n";	
+	}
+	else
+		echo "ERROR\n";	
 }
 else 
 {
-	if (!file_exists('private')) 
-		mkdir('private') or die("Can't create folder\n");	// quelles permissions ?
-	$account[] = ['login' => $_POST['login'], 'passwd' => hash('sha512' ,$_POST['passwd'])]; 
+	if (!file_exists('../htdocs/private')) 
+		mkdir('../htdocs/private') or die("Can't create folder\n");	// quelles permissions ?
+	$account[] = ['login' => $_POST['login'], 'passwd' => hash('whirlpool' ,$_POST['passwd'])]; 
 	file_put_contents($passwd_file, serialize($account))
 		or die("Can't write file\n");	
+	echo "OK\n";	
 }
-	
-print_r($account);
-
 ?>
