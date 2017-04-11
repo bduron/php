9f555436792ba8badc3ca001ff1148168d1369a3
Benjamin DURON <bduron@e2r9p20.42.fr>
2017-04-11 18:52:00 +0200

    j06 3

--- rendu/j06/ex02/Vector.class.php
+++ rendu/j06/ex02/Vector.class.php
@@ -8,50 +8,35 @@ class Vector
 	private $_y = 0;
 	private $_z = 0;
 	private $_w = 0.0;
-	private $_dest;
-	private $_orig;
-	
+
 	public static $verbose = FALSE; 
 
-	public function __construct(array $kwargs)	
-	{	
-		
-		if (!array_key_exists('orig', $kwargs))
-		{
-			$this->_x = $kwargs['dest']->getX();	
-			$this->_y = $kwargs['dest']->getY();	
-			$this->_z = $kwargs['dest']->getZ();	
-		}	
-		else 
-		{	$this->_x = $kwargs['dest']->getX() - $kwargs['orig']->getX();	
-			$this->_y = $kwargs['dest']->getY() - $kwargs['orig']->getX();	
-			$this->_z = $kwargs['dest']->getZ() - $kwargs['orig']->getX();	
-		}
-	//	if (self::$verbose)
-	//		printf("Vertex( x: %.2f, y: %.2f, z:%.2f, w:%.2f, %s ) constructed\n",
-	//			$this->getX(), $this->getY(), $this->getZ(), 
-	//			$this->getW(), $this->getColor());
+	public function __construct(array $kwargs)
+	{
+		$this->_dest = $kwargs['dest'];
+
+		$_orig = (key_exists('orig', $kwargs)) ? $kwargs['orig'] 
+			: new Vertex( array('x' => 0, 'y' => 0, 'z' => 0) );
+
+		$this->_x = $this->_dest->getX() - $_orig->getX();
+		$this->_y = $this->_dest->getY() - $_orig->getY();
+		$this->_z = $this->_dest->getZ() - $_orig->getZ();
+		$this->_w = 0;
+		if (self::$verbose)
+			printf("%s constructed\n", $this);
 		return ;
 	}
-	
-//	public function __toString()
-//	{
-//		if (self::$verbose) 
-//			return 	sprintf("Vertex( x: %.2f, y: %.2f, z:%.2f, w:%.2f, %s )",
-//					$this->getX(), $this->getY(), $this->getZ(), 
-//					$this->getW(), $this->getColor());
-//		else 
-//			return 	sprintf("Vertex( x: %.2f, y: %.2f, z:%.2f, w:%.2f )",
-//					$this->getX(), $this->getY(), $this->getZ(), 
-//					$this->getW());
-//	}
+
+	public function __toString()
+	{
+		return 	sprintf("Vector( x:%.2f, y:%.2f, z:%.2f, w:%.2f )",
+			$this->getX(), $this->getY(), $this->getZ(), $this->getW());
+	}
 
 	public function __destruct()
 	{
-//		if (self::$verbose) 
-//			printf("Vertex( x: %.2f, y: %.2f, z:%.2f, w:%.2f, %s ) destructed\n",
-//				$this->getX(), $this->getY(), $this->getZ(), 
-//				$this->getW(), $this->getColor());
+		if (self::$verbose)
+			printf("%s destructed\n", $this);
 		return ;
 	}
 
@@ -64,63 +49,62 @@ class Vector
 	public function getY() { return $this->_y; }
 	public function getZ() { return $this->_z; }
 	public function getW() { return $this->_w; }
-	 	
+
 	public function magnitude() 
 	{
 		return sqrt(pow($this->getX(), 2) + pow($this->getY(), 2) + pow($this->getZ(), 2));
 	}
 
-	/**************/
-	public function normalize() // ADDD the special CASE 
+	public function normalize()
 	{
-		$normalized = clone $this;
-		$normalized->_x	= $normalized->_x / magnitude(); 
-		$normalized->_y	= $normalized->_y / magnitude(); 
-		$normalized->_z	= $normalized->_z / magnitude(); 
-		return $normalized;
+		$vtx = new Vertex( array('x' => ($this->getX() / $this->magnitude()), 
+			'y' => ($this->getY() / $this->magnitude()), 
+			'z' => ($this->getZ() / $this->magnitude())) );
+
+		return (new Vector( array('dest' => $vtx) ));
 	}
 
 	public function add(Vector $rhs) 
 	{
-		$addition = clone $this;
-		$addition->_x	= $addition->_x + $rhs->getX(); 
-		$addition->_y	= $addition->_y + $rhs->getY();
-		$addition->_z	= $addition->_z + $rhs->getZ();
-		return $addition;
+		$vtx = new Vertex( array('x' => ($this->getX() + $rhs->getX()), 
+			'y' => ($this->getY() + $rhs->getY()), 
+			'z' => ($this->getZ() + $rhs->getZ())) );
+
+		return (new Vector( array('dest' => $vtx) ));
 	}
 
 	public function sub(Vector $rhs) 
 	{
-		$addition = clone $this;
-		$addition->_x	= $addition->_x - $rhs->getX(); 
-		$addition->_y	= $addition->_y - $rhs->getY();
-		$addition->_z	= $addition->_z - $rhs->getZ();
-		return $addition;
+		$vtx = new Vertex( array('x' => ($this->getX() - $rhs->getX()), 
+			'y' => ($this->getY() - $rhs->getY()), 
+			'z' => ($this->getZ() - $rhs->getZ())) );
+
+		return (new Vector( array('dest' => $vtx) ));
 	}
 
 	public function opposite() 
 	{
-		$opposite = clone $this;
-		$opposite->_x	= $opposite->_x * (-1); 
-		$opposite->_y	= $opposite->_y * (-1);
-		$opposite->_z	= $opposite->_z * (-1);
-		return $opposite;
+		$vtx = new Vertex( array('x' => (-1 * $this->getX()), 
+			'y' => (-1 * $this->getY()), 
+			'z' => (-1 * $this->getZ())) );
+
+		return (new Vector( array('dest' => $vtx) ));
 	}
 
 	public function scalarProduct($k)
 	{
-		$scalar = clone $this;
-		$scalar->_x	= $scalar->_x * $k; 
-		$scalar->_y	= $scalar->_y * $k;
-		$scalar->_z	= $scalar->_z * $k;
-		return $scalar;
+		$vtx = new Vertex( array('x' => ($k * $this->getX()), 
+			'y' => ($k * $this->getY()), 
+			'z' => ($k * $this->getZ())) );
+
+		return (new Vector( array('dest' => $vtx) ));
 	}
 
 	public function dotProduct(Vector $rhs)
 	{
 		return ($this->getX() * $rhs->getX())
-		   + ($this->getY() * $rhs->getY())	
-		   + ($this->getZ() * $rhs->getZ());
+			+ ($this->getY() * $rhs->getY())	
+			+ ($this->getZ() * $rhs->getZ());
 	}
 
 	public function cos(Vector $rhs)
@@ -130,13 +114,12 @@ class Vector
 
 	public function crossProduct(Vector $rhs)
 	{
-		$cross = clone $this;
-		$cross->_x	= $cross->_x * $k; 
-		$cross->_y	= $cross->_y * $k;
-		$cross->_z	= $cross->_z * $k;
-		return $cross;
+		$vtx = new Vertex( array('x' => ($this->getY() * $rhs->getZ() - $this->getZ() * $rhs->getY()), 
+			'y' => ($this->getZ() * $rhs->getX() - $this->getX() * $rhs->getZ()), 
+			'z' => ($this->getX() * $rhs->getY() - $this->getY() * $rhs->getX())) );
+
+		return (new Vector( array('dest' => $vtx) ));
 	}
 }
 
 ?>
-
