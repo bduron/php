ad455a371667164923b14d4a10b59ede4679eac8
Benjamin Duron (linux) <bduron@student.42.fr>
2017-04-13 11:52:48 +0200

    j07 ex04

---
+++ rendu/j07/ex03/House.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class House
+{
+
+	public function introduce()
+	{
+		print "House ".static::getHouseName()." of ".static::getHouseSeat()." : \"".static::getHouseMotto()."\"".PHP_EOL; 	
+	}
+}
+
+
+
+?>
